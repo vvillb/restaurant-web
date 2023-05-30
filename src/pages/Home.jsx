d20ae489b45f6e273d678ffd75c217e8a9abfe1f
Virginia Villalobos <virginiavt98@gmail.com>
2023-05-30 22:37:34 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Layout from '../ThemeLayout/Layout'
-import { Grid, Typography } from '@mui/material'
+import { Card, CardMedia, Grid, Typography } from '@mui/material'
 
 const Home = () => {
   return (
@@ -9,11 +9,30 @@ const Home = () => {
         sx={{
             backgroundColor:'background.main'
         }}>
-           <Typography variant='h1'
+          <Grid item>
+             <Typography variant='h1'
            sx={{
             paddingTop:'10vh',
             color:'highlights.blue'
            }}>Virbakes</Typography>
+          </Grid>
+          <Grid item>
+              <Grid item
+              sx={{
+                background: `url(${playa})`,
+                backgroundSize:'cover',       
+                height: '125vh',
+                display: 'flex',
+                justifyContent: 'flex-start',
+                alignItems: 'top',
+                width:'100vw',
+                flexDirection:'row',
+              }}>
+              </Grid>
+              <Grid item>
+              </Grid>
+          </Grid>
+          
         </Grid>
       
     </Layout>
